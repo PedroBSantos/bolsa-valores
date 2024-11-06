154a1c6d7e205e0d0447944410d7ed644ec63894
Pedro Santos <pedrobsantos98@outlook.com>
2024-11-05 21:41:54 -0300

    feature: add opentelemetry

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,16 @@ RUN lein with-profile docker uberjar
 FROM eclipse-temurin:21.0.4_7-jre-alpine AS final
 COPY --from=build /app/target/uberjar/bolsa-valores-0.1.0-SNAPSHOT-standalone.jar /app/bolsa-valores.jar
 WORKDIR /app/
-ENTRYPOINT java -Dconf="config/docker.edn" -jar ./bolsa-valores.jar
+RUN wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v2.9.0/opentelemetry-javaagent.jar
+ENTRYPOINT java -javaagent:./opentelemetry-javaagent.jar \
+    -Dotel.service.name=bolsa-valores \
+    -Dotel.exporter.otlp.endpoint=http://otlp-container:4317 \
+    -Dotel.instrumentation.http.client.enabled=true \
+    -Dotel.traces.exporter=otlp \
+    -Dotel.metrics.exporter=otlp \
+    -Dotel.logs.exporter=otlp \
+    -Dotel.exporter.otlp.protocol=grpc \
+    -Dconf="config/docker.edn" \
+    -Dclojure.tools.logging.factory=clojure.tools.logging.impl/log4j2-factory \
+    -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager \
+    -jar ./bolsa-valores.jar
--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [com.layerware/hugsql "0.5.3"]
                  [com.layerware/hugsql-adapter-next-jdbc "0.5.3"]
                  [org.postgresql/postgresql "42.7.3"]
@@ -13,16 +13,30 @@
                  [ring/ring-json "0.5.1"]
                  [compojure/compojure "1.7.1"]
                  [org.clojure/tools.logging "1.3.0"]
-                 [org.apache.logging.log4j/log4j-core "2.23.1"]
                  [ring/ring-defaults "0.4.0"]
                  [ring-logger/ring-logger "1.1.1"]
                  [org.clojure/data.json "2.5.0"]
-                 [org.slf4j/slf4j-simple "2.0.12"]
                  [clj-test-containers "0.7.4"]
-                 [ring-cors "0.1.13"]]
+                 [ring-cors "0.1.13"]
+                 [com.github.steffan-westcott/clj-otel-api "0.2.7"]
+                 [org.apache.logging.log4j/log4j-core "2.24.1"]
+                 [org.apache.logging.log4j/log4j-jcl "2.24.1"]
+                 [org.apache.logging.log4j/log4j-jul "2.24.1"]
+                 [org.apache.logging.log4j/log4j-slf4j2-impl "2.24.1"]]
   :main ^:skip-aot bolsa-valores.core
   :target-path "target/%s"
   :profiles {:dev {:aot :all
-                   :jvm-opts ["-Dclojure.compiler.direct-linking=true" "-Dconf=config/dev.edn"]}
+                   :jvm-opts ["-javaagent:resources/otel/opentelemetry-javaagent.jar"
+                              "-Dotel.service.name=bolsa-valores"
+                              "-Dotel.exporter.otlp.endpoint=http://localhost:4317"
+                              "-Dotel.instrumentation.http.client.enabled=true"
+                              "-Dotel.traces.exporter=otlp"
+                              "-Dotel.metrics.exporter=otlp" 
+                              "-Dotel.logs.exporter=otlp"
+                              "-Dotel.exporter.otlp.protocol=grpc"
+                              "-Dclojure.compiler.direct-linking=true" 
+                              "-Dconf=config/dev.edn"
+                              "-Dclojure.tools.logging.factory=clojure.tools.logging.impl/log4j2-factory"
+                              "-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager"]}
              :docker {:aot :all
                       :jvm-opts ["-Dclojure.compiler.direct-linking=true" "-Dconf=config/docker.edn"]}})
